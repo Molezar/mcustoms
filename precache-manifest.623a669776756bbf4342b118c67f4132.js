21845213582dbc9f81cc1755380f78d327bf634b
Molezar <61122341+Molezar@users.noreply.github.com>
2020-03-19 05:48:17 +0200

    Updates

---
+++ precache-manifest.623a669776756bbf4342b118c67f4132.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab7de2dd54de6bd5ec4ff67182b78f2e",
+    "url": "/mcustoms/index.html"
+  },
+  {
+    "revision": "ae7fe768847dc5c1210e",
+    "url": "/mcustoms/static/css/2.02ebff17.chunk.css"
+  },
+  {
+    "revision": "c6825a8b1909ab317cdf",
+    "url": "/mcustoms/static/css/main.fef6c296.chunk.css"
+  },
+  {
+    "revision": "ae7fe768847dc5c1210e",
+    "url": "/mcustoms/static/js/2.6fa0cba3.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/mcustoms/static/js/2.6fa0cba3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6825a8b1909ab317cdf",
+    "url": "/mcustoms/static/js/main.3b710cfc.chunk.js"
+  },
+  {
+    "revision": "8838d9027af3f5885c70",
+    "url": "/mcustoms/static/js/runtime-main.c53415a5.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/mcustoms/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/mcustoms/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/mcustoms/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/mcustoms/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/mcustoms/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/mcustoms/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/mcustoms/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/mcustoms/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/mcustoms/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/mcustoms/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/mcustoms/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/mcustoms/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/mcustoms/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/mcustoms/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/mcustoms/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/mcustoms/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "ac89033d98215aac0f8a9356f1f89d0f",
+    "url": "/mcustoms/static/media/zakat.ac89033d.jpg"
+  }
+]);
--- src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-
-const Header = () => (
-<div class="ui three item menu">
-    <a class="active item"><font color="blue">We are opened!</font></a>
-   </div>
-);
-export default Header;
